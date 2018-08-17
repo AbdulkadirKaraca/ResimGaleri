3cc8206432f610a1f11e9f0ff2240a2963bd595f
AbdulkadirKaraca <kadirkaraca1993@gmail.com>
2018-08-17 16:36:27 +0300

    Asp.NetCoreMVC
    Visual Studio 2017

---
+++ ResimGaleri.CoreMvcUI/Models/Movie.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResimGaleri.CoreMvcUI.Models
+{
+    public class Movie
+    {
+    }
+}
